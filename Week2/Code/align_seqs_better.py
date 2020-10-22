25e2cb10deefc5d51869605b26e36796c212d88c
Cong Liu <cong.liu20@imperial.ac.uk>
2020-10-22 11:38:51 +0100

    CMEECourseWork

--- Week2/Code/align_seqs_better.py
+++ Week2/Code/align_seqs_better.py
@@ -1,63 +1,104 @@
-"""Language: Python3
-   Auther: Cong Liu (cong.liu20@imperial.ac.uk)
-   Script: align_seqs_better.py
-   Work Path: CMEECourseWork
-   Input: Two fasta files saved in CMEECourseWork/Week1/Data
-   Function: Align two sequences saved in input files. 
-   Output: All best alignments with equal and highest score, saved in Results/align.txt
-   Usage: python align_seqs_better.py [file1] [file2]
-   Date: Oct, 2020"""
+#!/usr/bin/env python3
 
-#importing
-import sys
+"""
+Script that takes DNA sequences as input from a single external file and 
+aligns two DNA sequences such that they are as similar as possible. The best 
+alignment, along with its corresponding score is then saved in a text file to 
+the /Results/ directory. 
 
-#Save input file names as a list
-files = sys.argv[1:]
+This script is able to take user arguments from cli. However, if no arguments 
+were provided, the script defaults to two fasta files in the /Data/ directory.
 
-def fasta(f):
-    """convert fasta file to a list with such form: 
-       [sequence name start with ">", sequence, ...]"""
-    file = open("Week1/Data/" + str(f), "r")
-    list1 = []
-    for line in file:
-        line = line.strip()
-        list1.append(line)
-    file.close()
+Script starts by positioning the beginning of the shorter sequence at all 
+positions (bases) of the longer one (the start position), and count the number 
+of bases matched. The alignment with the highest score wins. Ties are possible, 
+in which case, an arbitrary alignment (e.g. first or last) with the highest 
+score is taken.
 
-    list2 = []
-    for i in range(0, len(list1)):
-        if list1[i][0] == ">":
-            list2.append(list1[i])
-            seq = ""
-            for j in range(i + 1, len(list1)):
-                if list1[j][0] != ">":
-                    seq = seq + list1[j]
-                else:
-                    break
-            list2.append(seq)
-    return list2        
-        
+This script also saves equally good alignments when they share the same score as
+the best alignment.
+"""
+
+__appname__ = '[align_seqs_better.py]'
+__author__ = 'Yewshen Lim (y.lim20@imperial.ac.uk)'
+__version__ = '0.0.1'
+__license__ = ""
+
+## Imports ##
+import sys # module to interface our program with the operating system
+import pickle
+
+## Constants ##
+
+# Read cli arguments using the sys module
+
+if len(sys.argv) != 3:
+    print("No arguments provided, defaulting to 2 fasta files in ../Data/")
+    # opens the default files
+    with open('../Data/407228326.fasta', 'r') as f1:
+        seq1 = [line.strip() for line in f1]
+    with open('../Data/407228412.fasta', 'r') as f2:
+        seq2 = [line.strip() for line in f2]
+else:
+    # opens the files provided as arguments
+    with open(sys.argv[1], 'r') as f1:
+        seq1 = [line.strip() for line in f1]
+    with open(sys.argv[2], 'r') as f2:
+        seq2 = [line.strip() for line in f2]
+
+# Populate s1 and s2 with sequences from file1 and file2 respectively
+# also skips the line if it starts with > (skips lines which are not sequences)
+s1 = []
+for row in seq1:
+    if row[0:1] != '>':
+        row = row.strip() # removes '\n'
+        s1.append(row)
+
+s2 = []
+for row in seq2:
+    if row[0:1] != '>':
+        row = row.strip() # removes '\n'
+        s2.append(row)
+
+# convert the lists to strings
+s1 = "".join(s1)
+s2 = "".join(s2)
+
 # Assign the longer sequence s1, and the shorter to s2
 # l1 is length of the longest, l2 that of the shortest
-seq1 = fasta(files[0])[1]
-seq2 = fasta(files[1])[1]
 
-
-l1 = len(seq1)
-l2 = len(seq2)
-if l1 >= l2:
-    s1 = seq1
-    s2 = seq2
-else:
-    s1 = seq2
-    s2 = seq1
+l1 = len(s1)
+l2 = len(s2)
+if l1 <= l2:
+    s1, s2 = s2, s1 # swap the two seqences
     l1, l2 = l2, l1 # swap the two lengths
+
+# for finding the best match (highest score) for the two sequences
+
+my_best_align = None
+my_best_score = -1
+
+## Functions ##
 
 # A function that computes a score by returning the number of matches starting
 # from arbitrary startpoint (chosen by user)
 def calculate_score(s1, s2, l1, l2, startpoint):
-    """A function that computes a score by returning the number of matches starting
-       from arbitrary startpoint (chosen by user)"""
+    """
+    Computes a score by returning the number of matches starting from an 
+    arbitrary startpoint (chosen by user)
+
+        Parameters:
+            s1 (str): string containing the longer sequence
+            s2 (str): string containing the shorter sequence
+            l1 (int): length of s1
+            l2 (int): length of s2
+            startpoint: arbitrary startpoint chosen by user
+
+        Returns:
+            score (int): number of matches between the sequences
+            matched (str): matched string with * as match and - as no match
+
+    """
     matched = "" # to hold string displaying alignements
     score = 0
     for i in range(l2):
@@ -68,33 +109,58 @@ def calculate_score(s1, s2, l1, l2, startpoint):
             else:
                 matched = matched + "-"
 
+    # some formatted output, commented out to speed up the code
+    # print("." * startpoint + matched)           
+    # print("." * startpoint + s2)
+    # print(s1)
+    # print(score) 
+    # print(" ")
+
     return score
 
+# Test the function with some example starting points:
+# calculate_score(s1, s2, l1, l2, 0)
+# calculate_score(s1, s2, l1, l2, 1)
+# calculate_score(s1, s2, l1, l2, 5)
 
-#find the best matches (highest score) for the two sequences
-my_best_align = None
-scores = []
-startpoints = []
-for i in range(l1):
+# Open a file for pickle
+pf = open('../Results/all_good_aligns.txt', 'wb') # save pickle file as .p (or else might run into issues saving and reopening later)
+# Create an empty list to capture the best score and equally good alignments
+all_good = []
+
+for i in range(l1): # Note that you just take the last alignment with the highest score
     z = calculate_score(s1, s2, l1, l2, i)
-    scores.append(z)
-    startpoints.append(i)
+    if z > my_best_score:
+        my_best_align = "." * i + s2
+        my_best_score = z
+        all_good = [] # empty out scores which are worse than current one
+        all_good.append(my_best_align)
+        all_good.append(my_best_score)
+    elif z == my_best_score:
+        my_best_align = "." * i + s2
+        my_best_score = z
+        all_good.append(my_best_align)
+        all_good.append(my_best_score)
 
-my_best_score = sorted(scores)[-1]
-my_best_aligns = []
-for i in range(0, len(scores)):
-    if scores[i] == my_best_score:
-        my_best_align = "." * startpoints[i] + s2
-        my_best_aligns.append(my_best_align)
+# write all_good to pf
+pickle.dump(all_good, pf)
+pf.close()
 
-print(my_best_aligns)
+print(my_best_align)
+print(s1)
 print("Best score:", my_best_score)
 
-#Save output
-output = open("Week2/Results/align.txt", "w")
-output.write(">Best score: " + str(my_best_score) + "\n")
-for align in my_best_aligns:
-    output.write(align + "\n")
-    output.write(s1 + "\n" * 2)
-
-output.close()
+# Save output to a text file in /Results/ directory
+sys.stdout = open('../Results/best_align.txt', 'w')
+pf = open('../Results/all_good_aligns.txt', 'rb')
+all_good = pickle.load(pf)
+print("Best score is:", str(all_good[1]))
+print("Alignments with such score:")
+# for loop to write all the equally good alignments (if > 1) to the output file
+for i in range(len(all_good)):
+    if i % 2 == 0:
+        print(all_good[i])
+        print("")
+        print(s1)
+pf.close() # close pickle file
+sys.stdout.close() # close output file
--- Week2/Code/align_seqs_fasta.py
+++ Week2/Code/align_seqs_fasta.py
@@ -1,67 +1,99 @@
-"""Language: Python3
-   Auther: Cong Liu (cong.liu20@imperial.ac.uk)
-   Script: align_seqs_fasta.py
-   Work Path: CMEECourseWork
-   Input: Two fasta files saved in CMEECourseWork/Week1/Data
-   Function: Align two sequences saved in input files. 
-             If input files were not provided, the script would take Data/407228326.fasta and Data/407228412.fasta as default input.
-   Output: The best alignment and corresponding score, saved in Results/align.txt
-   Usage: python align_seqs_fasta.py [file1] [file2]
-   Date: Oct, 2020"""
+#!/usr/bin/env python3
 
-#importing
-import sys
+"""
+Script that takes DNA sequences as input from a single external file and 
+aligns two DNA sequences such that they are as similar as possible. The best 
+alignment, along with its corresponding score is then saved in a text file to 
+the /Results/ directory. 
 
-#Save input file names as a list
-files = sys.argv[1:]
+This script is able to take user arguments from cli. However, if no arguments 
+were provided, the script defaults to two fasta files in the /Data/ directory.
 
-def fasta(f):
-    """A function that convert fasta file to a list with such form: 
-    [sequence name start with ">", sequence, ...]"""
-    file = open("Week1/Data/" + str(f), "r")
-    list1 = []
-    for line in file:
-        line = line.strip()
-        list1.append(line)
-    file.close()
+Script starts by positioning the beginning of the shorter sequence at all 
+positions (bases) of the longer one (the start position), and count the number 
+of bases matched. The alignment with the highest score wins. Ties are possible, 
+in which case, an arbitrary alignment (e.g. first or last) with the highest 
+score is taken.
+"""
 
-    list2 = []
-    for i in range(0, len(list1)):
-        if list1[i][0] == ">":
-            list2.append(list1[i])
-            seq = ""
-            for j in range(i + 1, len(list1)):
-                if list1[j][0] != ">":
-                    seq = seq + list1[j]
-                else:
-                    break
-            list2.append(seq)
-    return list2        
-        
+__appname__ = '[align_seqs_fasta.py]'
+__author__ = 'Yewshen Lim (y.lim20@imperial.ac.uk)'
+__version__ = '0.0.1'
+__license__ = ""
+
+## Imports ##
+import sys # module to interface our program with the operating system
+
+## Constants ##
+
+# Read cli arguments using the sys module
+
+if len(sys.argv) != 3:
+    print("No arguments provided, defaulting to 2 fasta files in ../Data/")
+    # opens the default files
+    with open('../Data/407228326.fasta', 'r') as f1:
+        seq1 = [line.strip() for line in f1]
+    with open('../Data/407228412.fasta', 'r') as f2:
+        seq2 = [line.strip() for line in f2]
+else:
+    # opens the files provided as arguments
+    with open(sys.argv[1], 'r') as f1:
+        seq1 = [line.strip() for line in f1]
+    with open(sys.argv[2], 'r') as f2:
+        seq2 = [line.strip() for line in f2]
+
+# Populate s1 and s2 with sequences from file1 and file2 respectively
+# also skips the line if it starts with > (skips lines which are not sequences)
+s1 = []
+for row in seq1:
+    if row[0:1] != '>':
+        row = row.strip() # removes '\n'
+        s1.append(row)
+
+s2 = []
+for row in seq2:
+    if row[0:1] != '>':
+        row = row.strip() # removes '\n'
+        s2.append(row)
+
+# convert the lists to strings
+
+s1 = "".join(s1)
+s2 = "".join(s2)
+
 # Assign the longer sequence s1, and the shorter to s2
 # l1 is length of the longest, l2 that of the shortest
-if len(files) != 0:
-    seq1 = fasta(files[0])[1]
-    seq2 = fasta(files[1])[1]
-else:
-    seq1 = fasta("407228326.fasta")[1]
-    seq2 = fasta("407228412.fasta")[1]
 
-
-l1 = len(seq1)
-l2 = len(seq2)
-if l1 >= l2:
-    s1 = seq1
-    s2 = seq2
-else:
-    s1 = seq2
-    s2 = seq1
+l1 = len(s1)
+l2 = len(s2)
+if l1 <= l2:
+    s1, s2 = s2, s1 # swap the two seqences
     l1, l2 = l2, l1 # swap the two lengths
 
+# for finding the best match (highest score) for the two sequences
+
+my_best_align = None
+my_best_score = -1
+
+## Functions ##
 
 def calculate_score(s1, s2, l1, l2, startpoint):
-    """A function that computes a score by returning the number of matches starting
-       from arbitrary startpoint (chosen by user)"""
+    """
+    Computes a score by returning the number of matches starting from an 
+    arbitrary startpoint (chosen by user)
+
+        Parameters:
+            s1 (str): string containing the longer sequence
+            s2 (str): string containing the shorter sequence
+            l1 (int): length of s1
+            l2 (int): length of s2
+            startpoint: arbitrary startpoint chosen by user
+
+        Returns:
+            score (int): number of matches between the sequences
+            matched (str): matched string with * as match and - as no match
+
+    """
     matched = "" # to hold string displaying alignements
     score = 0
     for i in range(l2):
@@ -72,27 +104,33 @@ def calculate_score(s1, s2, l1, l2, startpoint):
             else:
                 matched = matched + "-"
 
+    # some formatted output, commented out to speed up the code
+    # print("." * startpoint + matched)           
+    # print("." * startpoint + s2)
+    # print(s1)
+    # print(score) 
+    # print(" ")
+
     return score
 
-
-#find the best match (highest score) for the two sequences
-my_best_align = None
-my_best_score = -1
+# Test the function with some example starting points:
+# calculate_score(s1, s2, l1, l2, 0)
+# calculate_score(s1, s2, l1, l2, 1)
+# calculate_score(s1, s2, l1, l2, 5)
 
 for i in range(l1): # Note that you just take the last alignment with the highest score
     z = calculate_score(s1, s2, l1, l2, i)
     if z > my_best_score:
-        my_best_align = "." * i + s2
+        my_best_align = "." * i + s2 # think about what this is doing!
         my_best_score = z 
+
 print(my_best_align)
 print(s1)
 print("Best score:", my_best_score)
 
-#Save output
-output = open("Week2/Results/align.txt", "w")
-
-output.write(my_best_align + "\n")
-output.write(s1 + "\n")
-output.write("Best score: " + str(my_best_score))
-
-output.close()
+# Save output to a text file in /Results/ directory
+sys.stdout = open('../Results/best_align.txt', 'w')
+# Write to the file
+print("Best align is:", str(my_best_align))
+print("Best score is:", str(my_best_score))
+sys.stdout.close() # Close output file
